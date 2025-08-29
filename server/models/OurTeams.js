9f022012f24728ef5422cc1560e5d69ff4c8c504
StephenBrooks714 <stephenbrooks714@gmail.com>
2025-08-28 19:01:21 -0500

    update

---
+++ server/models/OurTeams.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+    name: String,
+    role: String,
+    description: String,
+    image: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    },
+    createdAt: String,
+    updatedAt: String
+})
+
+module.exports = mongoose.model('team', teamSchema);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const cache = require("../config/cache");
+const auth = require("../middleware/ifAuthorized");
 
 const mainController = require("../controllers/mainPageController");
 router.get("/", mainController.homePage);
 router.get("/boardMembers", mainController.boardMembersPage);
 router.get("/ourReach", mainController.ourReachPage);
 
-
+const userActionController = require("../controllers/users/index");
+router.get("/register", cache(2), userActionController.registerUser);
+router.post("/store/user", cache(2), userActionController.storeUser);
+router.get("/login", cache(2), userActionController.loginPage);
+router.post("/login/user", cache(2), userActionController.loginUserAction);
+router.get("/logout", cache(2), userActionController.logoutUser);
+router.get("/updateUser/:id", cache(2), auth, userActionController.updateUser);
+router.post("/updateUser/:id", cache(2), userActionController.postUpdatedUser);
+router.get("/deleteUser/:id", cache(2), auth, userActionController.deleteUser);
 
 module.exports = router;
